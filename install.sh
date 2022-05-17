e519b23f7a27f16c5b1113f3ea33f963cd0d7341
Adam Paulukanis <adam.paulukanis@gmail.com>
2022-05-17 07:22:25 +0100

    Changes in install.sh
    - enable errexit (`set -e`): when error do exit
    - add emoji-test.txt file along with UTF-8-demo.txt is now easy to test
      if I have properly installed font which is OK with displaying UTF-8
          (and emoji)
    - fix formatting: tab is 4 spaces?

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-#set -e
+set -e
 
 OLDPWD=`pwd`
 MYPATH="$HOME/dotHome/"
-WHITELIST="UTF-8-demo.txt Xdefaults Xresources gitconfig profile tmux.conf vimrc vim xsession"
+WHITELIST="UTF-8-demo.txt emoji-test.txt Xdefaults Xresources gitconfig profile tmux.conf vimrc vim xsession"
 
 cd $MYPATH
 
 # gf good-file
 for gf in $WHITELIST
 do
-	ln -s "$MYPATH$gf" "$HOME/.${gf##*/}"
+    ln -s "$MYPATH$gf" "$HOME/.${gf##*/}"
 done
 
 cd $OLDPWD
