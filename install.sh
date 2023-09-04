5d0e8675c891c3729a0df00e8a8eb308b548b743
Adam Paulukanis <adam.paulukanis@gmail.com>
2023-09-05 00:36:47 +0100

    install.sh: changes

--- install.sh
+++ install.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-#set -e
+set -e
 
 OLDPWD=`pwd`
-MYPATH="$HOME/dotHome/"
 WHITELIST="UTF-8-demo.txt emoji-test.txt Xdefaults Xresources gitconfig profile tmux.conf vimrc vim xsession ctags npmrc"
 
 kaszana sie dzieje, gdy plik juz istnieje, bo nie jest linkowany. Np. .profile istnieje i co wtedy?
 moze niech robi kopie takiego pliku (w jakims ~/backup folderze) [mv file] i wtedy zadziala?
 
-cd $MYPATH
-
 # gf good-file
 for gf in $WHITELIST
 do
-    ln -s "$MYPATH$gf" "$HOME/.${gf##*/}"
+    #ln -s "$MYPATH$gf" "$HOME/.${gf##*/}"
+    ln -s $gf "$HOME/.${gf##*/}"
 done
-
-cd $OLDPWD
 
 # NPM
 mkdir "${HOME}/.npm-global"
+
+# i3 files
