c30d1b7ddc3804013d07a47faf8f3b73fe853c02
Adam Paulukanis <adam.paulukanis@gmail.com>
2024-09-24 11:25:48 +0100

    Generate and use system tags (/usr/include) in Vim
    - each system update generates a new systags file
    - vim uses it
    - don't track this file

--- bin/update-system.sh
+++ bin/update-system.sh
@@ -7,3 +7,6 @@ pkg_delete -a
 pkg_add -uUI -D snap
 sysupgrade -s -n
 '
+
+# Build tags for C lang to be used in Vim. /usr/include contains only H files.
+ectags --c-kinds=+cdefglmnpstuvx -f /home/adam/.vim/systags /usr/include/*.h
