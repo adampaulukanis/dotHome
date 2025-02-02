5ed2ffc003e5a0179c099905b9955d7443ac157d
Adam Paulukanis <adam.paulukanis@gmail.com>
2025-02-02 17:36:08 +0000

    bin scripts: improvement of check-connection.sh

--- bin/check-connection.sh
+++ bin/check-connection.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-doas -u root /sbin/ifconfig run0 down
-sleep 1
-doas -u root /sbin/ifconfig run0 up
-sleep 1
-doas -u root /bin/sh /etc/netstart run0
+if ping -c1 wp.pl >/dev/null 2>/dev/null
+then
+    echo "OK!"
+    exit
+else
+    # ping not working, so there is a problem with network
+    doas -u root /sbin/ifconfig run0 down
+    sleep 1
+    doas -u root /sbin/ifconfig run0 up
+    sleep 1
+    doas -u root /bin/sh /etc/netstart run0
+fi
